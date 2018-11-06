a6e7b4d9fd97f204ba3c63ccd97b34d15e6381f8
Li Chen <chen1649chenli@gmail.com>
2018-11-06 12:21:27 -0600

    finished lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,14 +83,18 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         if (A == null) {
-            A = B;
+            return B;
         }
-        IntList ptr = A;
-        while (ptr.rest != null){
-            ptr = ptr.rest;
+        else if (B == A){
+            return A;
+        } else {
+            IntList ptr = A;
+            while (ptr.rest != null){
+                ptr = ptr.rest;
+            }
+            ptr.rest = B;
+            return A;
         }
-        ptr.rest = B;
-        return A;
     }
 
     /**
